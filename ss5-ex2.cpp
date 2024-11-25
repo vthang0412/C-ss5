90f90f51a97498a13bd7461a64f76b474371f762
vthang0412 <vthang041206@gmail.com>
2024-11-25 20:10:20 +0700

    Add files via upload

---
+++ ss5-ex2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int soChotruoc=10, soNhap;
+	 while (1) {
+        printf("Nhap mot so nguyen: ");
+        scanf("%d", &soNhap); 
+        break; 
+    }
+	
+	return 0;
+}
