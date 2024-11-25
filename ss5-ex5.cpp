48bb5d139cdb536172c6f85fb7b44a6c4509030e
vthang0412 <vthang041206@gmail.com>
2024-11-25 20:10:54 +0700

    Add files via upload

---
+++ ss5-ex5.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int i,n;
+	printf("Tat ca cac bang cuu chuong tu 1 den 9\n");
+	for (int i=1;i<=9;i++){
+		printf("Bang cuu chuong %d\n",i);
+	for (int n=1;n<=10;n++){
+		printf("%d x %d= %d\n",i,n,i * n);
+	}
+	}
+	return 0;
+}
